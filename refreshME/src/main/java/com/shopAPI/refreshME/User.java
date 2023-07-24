9f42f21163a6de81dabeeb6037c12355588f0b07
cioba <ciobanumateiciprian@gmail.com>
2023-07-24 22:42:53 +0300

    test postman

--- refreshME/src/main/java/com/shopAPI/refreshME/User.java
+++ refreshME/src/main/java/com/shopAPI/refreshME/User.java
@@ -5,10 +5,30 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document(collection = "User")
+@Document("User")
 public class User {
     @Id
-    private String id;
+    private Long id;
     private String userName;
 
+    public User(Long id, String userName) {
+        this.id = id;
+        this.userName = userName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
--- refreshME/src/main/java/com/shopAPI/refreshME/controller/UserController.java
+++ refreshME/src/main/java/com/shopAPI/refreshME/controller/UserController.java
@@ -1,4 +1,38 @@
 package com.shopAPI.refreshME.controller;
 
+import com.shopAPI.refreshME.User;
+import com.shopAPI.refreshME.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController // RestController is like a magic keyword that tells the Spring Framework that this class is meant to handle requests and return responses
 public class UserController {
+    UserService userService;
+
+    @Autowired
+    //whenever we use the userService variable, Spring will provide the necessary "UserService" object for us to work with.
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public Optional<User> getUserByName(@RequestParam(value = "id") Long id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping("/user/all")
+    public List<User> getAll() {
+        return userService.getAll();
+    }
+
+    @PostMapping("/user")
+    public void saveUserByName(@RequestParam(value = "id") Long id, @RequestParam(value = "name") String name) {
+        userService.saveUser(id, name);
+    }
 }
